7f60dd2f06457e69f6742e7cbf1ac5c04c138e5c
lbros96 <44737203+lbros96@users.noreply.github.com>
2019-01-02 17:49:05 -0500

    Add files via upload

---
+++ Booking.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Discover Homepage</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div style="width:100%">
+  <div style="width:100%; margin-bottom: -5px" >
+      <img src="image/homehead.jpg" width="100%">
+
+  </div>
+  <div style="background-color:#2AFC98;height: 750px; width:13%;float:left; margin-right: 2%">
+      <div><h3><b>Menu</b></h3></div>
+      <a href="Homepage1.html">Home</a><br />
+      <a href="Guides.html">Our Guides</a><br />
+      <a href="Contact.html">Contact</a><br />
+
+  </div>
+
+  <div style=";height: 360px; width:43%;float:left; margin-right: 2%; margin-top: 5px">
+        Thanks for letting us know your coming <?php echo $_GET["lastname"]; ?>, see you soon! 
+
+  </div>
+
+
+  
+<div style="background-color:#51BBFE;clear:both">
+  <center>
+      <a href="Homepage1.html">Home</a>|
+      <a href="Guides.html">Our Guides</a>|
+      <a href="Contact.html">Contact</a>
+  </center>
+  </div>
+</div>
+</body>
+</html>
